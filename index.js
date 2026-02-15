2e15d9d0f4624e699e02d598393e8c55f7b52a69
achekavy <achekavy@gmail.com>
2026-02-15 17:44:55 +0100

    new tags

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ window.startBanner = function () {
     path: "data.json",
   });
 };
+
+(function () {
+  var industryBlock = document.getElementById("industry-block");
+  var metricTags = document.querySelectorAll(".metric-tag");
+  var industryTags = document.querySelectorAll(".industry-tag");
+
+  function selectOnlyOne(buttons, selectedClass) {
+    return function (ev) {
+      var target = ev.currentTarget;
+      buttons.forEach(function (btn) {
+        btn.classList.remove(selectedClass);
+      });
+      target.classList.add(selectedClass);
+    };
+  }
+
+  metricTags.forEach(function (btn) {
+    btn.addEventListener("click", function (ev) {
+      selectOnlyOne(metricTags, "tag-selected")(ev);
+      if (industryBlock) industryBlock.classList.remove("hidden");
+    });
+  });
+
+  industryTags.forEach(function (btn) {
+    btn.addEventListener("click", selectOnlyOne(industryTags, "tag-selected"));
+  });
+})();
